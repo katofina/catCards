9df6cd1983aa63de24380fd0fe3bebdc8ea84d3d
katofina <lizuha1860@gmail.com>
2025-02-05 10:47:26 +0100

    initializing of project, main

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <header>
+
+      </header>
+      <body>
+      <section>
+        <h1>Kittens that melt your heart</h1>
+      </section>
+      </body>
+    </div>
+  );
+}
+
+export default App;
